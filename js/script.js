de4ad71351123aa4e8c9515eee14ca1a6d28520c
Angelino Pires de Oliveira <110253939+Angelino-Pires-Oliveira@users.noreply.github.com>
2022-12-19 18:46:49 -0300

    finalizado

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var overlay = document.getElementById('overlay');
+var menu_hambur = document.getElementById('menu-hamb');
+function menu(){
+    overlay.classList.toggle('overlay-active');
+    menu_hambur.classList.toggle('menu-active');
+}
